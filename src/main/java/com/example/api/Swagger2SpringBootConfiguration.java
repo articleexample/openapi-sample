852d83dff0465c1fee5a26d588001a0f4290b643
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2018-11-10 01:18:31 +0530

    creating semi-adjusted

---
+++ src/main/java/com/example/api/Swagger2SpringBootConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger2SpringBootConfiguration {
+	
+	@Bean
+	FamilyApiDelegate familyApiDelegate()
+	{
+		return new FamilyApiDelegateImpl();
+	}
+	@Bean
+	PeopleApiDelegate peopleApiDelegate()
+	{
+		return new PeopleApiDelegateImpl();
+	}
+	
+
+}
